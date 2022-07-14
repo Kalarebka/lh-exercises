aed7d0233fae4a05dca22134cc31249bce1a5b5b
Ewelina Sitarczyk <aivellyn8@gmail.com>
2022-07-14 12:20:25 +0200

    Start user_management

--- OOP/user_management.py
+++ OOP/user_management.py
@@ -1,4 +1,4 @@
-# Create 4 clases: User, Administrator or Admin, Redactor and Post
+# Create 4 classes: User, Administrator or Admin, Redactor and Post
 # User class has to have name, surname, email, date of birth, gender, it has possibility to change own email, create, edit, delete own posts
 # Redactor class has all functions that User class has and has possibility to edit all posts
 # Admin class can edit everything, attributes of classes User and Redactor and can edit all posts
@@ -12,19 +12,64 @@
 
 # Use Abstract classes to provide solution
 # Simulate this same functionality with one class and permissions system, where Admin can elevate permissions of other users
+from abc import ABC, abstractmethod
+from datetime import date
+
+class AbstractUser(ABC):
+    def __init__(self, name, surname, email, date_of_birth, gender):
+        self.name = name
+        self.surname = surname
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.gender = gender
+        self.posts = []
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self.name == other.name and self.surname = other.surname
+
+    def create_post(self, content):
+        self.posts.append(Post(self, content))
+
+    def change_email(self, new_email):
+        self.email = new_email
+
+    @abstractmethod
+    def edit_post(self, post):
+        pass
+
+    @abstractmethod
+    def delete_post(self, post):
+        pass
+
+    
+class User(AbstractUser):
+    def edit_post(self, post, new_content):
+        if post.author == self:
+            post.edit(self, new_content)
+        else:
+            print("Permission denied")
 
 
-class User:
-    ...
+class Admin(AbstractUser):
+    def edit_post(self, post, new_content):
+        post.edit(self, new_content)
 
 
-class Admin:
-    ...
-
-
-class Redactor:
-    ...
+class Redactor(AbstractUser):
+    def edit_post(self, post, new_content):
+        post.edit(self, new_content)
 
 
 class Post:
-    ...
+    def __init__(self, author, content):
+        self.author = author
+        self.content = content
+        self.date_created = date.today() # could also do datetime here to record precise time
+        self.date_modified = self.date_created
+
+    # TODO comparisons of posts by content length
+
+    def edit(self, new_content):
+        self.content = new_content
+        self.date_modified = date.today()
+
