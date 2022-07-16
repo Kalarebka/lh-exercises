0ce7cb2054f8967276932e41ded98d94c21f8550
Ewelina Sitarczyk <aivellyn8@gmail.com>
2022-07-16 11:34:15 +0200

    Context manager not working

--- OOP/context_manager.py
+++ OOP/context_manager.py
@@ -3,4 +3,29 @@
 
 
 class FileHandler:
-    ...
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+        
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self
+
+    def get_line(self):
+        eof = False
+        while not eof:
+            line = self.file.readline()
+            if line:
+                yield line
+            else:
+                eof = True
+
+    def __exit__(self):
+        self.file.close()
+
+
+with FileHandler("trees.csv", "r") as f:
+    for i in range(10):
+        print(f.get_line())
+
