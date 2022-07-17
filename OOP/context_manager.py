b88d6c97072c551aad90e1fa549d7f7d18710d18
Ewelina Sitarczyk <aivellyn8@gmail.com>
2022-07-17 09:45:47 +0200

    Context manager still not working

--- OOP/context_manager.py
+++ OOP/context_manager.py
@@ -1,27 +1,20 @@
 # Create object FileHandler, which will serve as context manager for csv files
 # This manager should serve one line at a time on demand (use generator)
-
+import csv
 
 class FileHandler:
-    def __init__(self, filename, mode):
+    def __init__(self, filename):
         self.filename = filename
-        self.mode = mode
-        self.file = None
+        self.mode = "r"
+        self.csv_reader = None
         
     def __enter__(self):
-        self.file = open(self.filename, self.mode)
-        return self
+        file = open(self.filename, self.mode)
+        self.csv_reader = csv.reader(self.file, delimiter=",")
+        return self.csv_reader
 
-    def get_line(self):
-        eof = False
-        while not eof:
-            line = self.file.readline()
-            if line:
-                yield line
-            else:
-                eof = True
 
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_value, exc_tb):
         self.file.close()
 
 
